79878ef852b2953fb7ac6c7495390d1ea972f19d
ivivekpaswan <142706719+ivivekpaswan@users.noreply.github.com>
2024-03-26 17:23:05 +0530

    Add files via upload

---
+++ Borrower.java
@@ -0,0 +1,56 @@
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "borrowers")
+public class Borrower {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date borrowedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book borrowedBook;
+
+    // Constructors
+    public Borrower() {
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBorrowedDate() {
+        return borrowedDate;
+    }
+
+    public void setBorrowedDate(Date borrowedDate) {
+        this.borrowedDate = borrowedDate;
+    }
+
+    public Book getBorrowedBook() {
+        return borrowedBook;
+    }
+
+    public void setBorrowedBook(Book borrowedBook) {
+        this.borrowedBook = borrowedBook;
+    }
+}
